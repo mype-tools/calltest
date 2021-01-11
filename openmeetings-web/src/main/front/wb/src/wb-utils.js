9df80707f915e6496d50560db6c4f6ed86b23997
Marcel Theiner <theiner07@gmail.com>
2021-01-11 17:50:20 +0100

    set jitsi

--- openmeetings-web/src/main/front/wb/src/wb-utils.js
+++
@@ -1,9 +0,0 @@
-/* Licensed under the Apache License, Version 2.0 (the "License") http://www.apache.org/licenses/LICENSE-2.0 */
-
-module.exports = {
-	safeRemove: (e) => {
-		if (typeof(e) === 'object') {
-			e.remove();
-		}
-	}
-};
